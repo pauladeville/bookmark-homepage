d3fde30304410ec05c7a65a61ee972c78efb40f4
P <paula.deville@posteo.net>
2022-09-11 12:26:58 +0200

    features OK

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* MOBILE NAV */
 const burger = document.getElementById("burger");
 const nav = document.getElementById("menu");
 const socialIcons = document.getElementById("social");
@@ -5,3 +6,21 @@ burger.addEventListener("click", () => {
   nav.classList.toggle("active");
   socialIcons.classList.toggle("active");
 });
+/* SLIDESHOW FEATURES*/
+let slideIndex = 0;
+function currentSlide(n) {
+  showSlide((slideIndex = n));
+}
+function showSlide(slideIndex) {
+  let slides = document.getElementsByClassName("slider__slide");
+  let links = document.getElementsByClassName("slider__links");
+  for (let slide of slides) {
+    slide.classList.remove("shown");
+  }
+  slides[slideIndex].classList.add("shown");
+  for (let link of links){
+    link.classList.remove("active");
+  }
+  links[slideIndex].classList.add("active");
+}
+showSlide(slideIndex);
